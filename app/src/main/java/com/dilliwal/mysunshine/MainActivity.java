2193ebbb9994cea88bf4ae298898899d769098e8
abhidilliwal <mail@dilliwal.com>
2015-11-13 08:44:56 +0100

    showing dummy list data in list view

--- app/src/main/java/com/dilliwal/mysunshine/MainActivity.java
+++ app/src/main/java/com/dilliwal/mysunshine/MainActivity.java
@@ -2,12 +2,17 @@ package com.dilliwal.mysunshine;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/dilliwal/mysunshine/MainFragment.java
+++ app/src/main/java/com/dilliwal/mysunshine/MainFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -15,7 +21,7 @@ import android.view.ViewGroup;
  */
 public class MainFragment extends Fragment {
 
-
+    ArrayAdapter<String> arrAdapter;
 
     public MainFragment() {
         // Required empty public constructor
@@ -26,7 +32,29 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        arrAdapter = new ArrayAdapter<String>(this.getActivity(),
+               R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+        listView.setAdapter(arrAdapter);
+
+        return rootView;
     }
 
 
